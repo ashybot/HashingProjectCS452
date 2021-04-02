c0e4a29e3caef3bd6f501c40418fe248a6a76188
nrkieffer <kieff001@cougars.csusm.edu>
2021-04-01 21:45:04 -0400

    Removing comments

--- bruteF.py
+++ bruteF.py
@@ -1,23 +1,11 @@
-''' the needed libraries for this code to be able to open files 
-from a remote URL and hash password guesses into SHA-1. To 
-include them, create a new Python3 file in your IDE and type the 
-following into the first line'''
+''' 
+urllib : http related libary
+urllib.urlopen : open file from url
+hashlib : hash algorithms to use in code
+'''
 from urllib.request import urlopen, hashlib
 
-'''//this is where I want to make a read from local file work
-but still giving me trouble
-
-get hash from user**this is normally where you pull from url**
-print("enter password to crack: ")
-sha1hash = input()
-print(sha1hash)
-
-open a file full of passwords
-f = open ("test.txt", "r")
-print(f.read())
-
-#LIST_OF_COMMON_PASSWORDS = str(f)
-'''
+#get hash from user to crack
 sha1hash=input("Enter input the hash to crack : ")
 LIST_OF_COMMON_PASSWORDS = str(urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt').read(), 'utf-8')
 
