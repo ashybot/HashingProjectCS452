e05937a8a0d13641372bc083fcdc413dcf43a538
Samm-I-Am <lopez440@cougars.csusm.edu>
2021-04-08 22:47:32 -0400

    Added if statements to select one of the three algorithms

--- PasswordKraken.py
+++ PasswordKraken.py
@@ -5,6 +5,9 @@ hashlib : hash algorithms to use in code
 '''
 from urllib.request import urlopen, hashlib
 
+# Gets they type of hash from the user 
+hash_type = input("Select a type of Hash: 1.MD5  2.SHA-1  3.SHA-256")
+
 # Step 1: Request hashes to crack from the user 
 hash_file_name=input("Enter the name of the .txt file that holds the hashes to crack: ")
 # open the hash file provided by the user
@@ -24,8 +27,28 @@ for current_hash in sha1_hash_file:
     #print("this is whats in guess", guess)
 
 # Step 4: Hash the password chosen in Step 3
-    hashedGuess = hashlib.sha1(bytes(guess,'utf-8')).hexdigest()
+# Step 4.5: Depending on the hash chosen at first, use the necessary function
+
+# MD5 Hashing
+if (hash_type == 1) {
+	# Need code for MD5
+}
+# SHA-1 Hashing
+else if (hash_type == 2) {
+	hashedGuess = hashlib.sha1(bytes(guess,'utf-8')).hexdigest()
     #print("this is whats in hashedGuess", hashedGuess)
+}
+# SHA-256 Hashing
+else if (hash_type == 2) {
+	# Need code for SHA-256
+
+}
+# User is trolling us
+else {
+	print("Incorrect Hash selected")
+	quit()
+}
+    
 
 # Step 5: Compare the hash generated from Step 4 to the current hash from the user being examined
     # 5.1 - If we find a match, print the successful password
