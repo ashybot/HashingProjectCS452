a7b5c293e13de849d88c38c070716edd63dc0ff6
Sammuel Licea <lopez440@cougars.csusm.edu>
2021-05-06 15:46:54 -0700

    Finally working for all hashing algo

--- MD5Cracker.py
+++ MD5Cracker.py
@@ -8,7 +8,7 @@ from urllib.request import urlopen
 #import codecs
 
 #Start clock
-start = time.time()
+
 
 #solution = str(sys.argv[1])
 #guess = " "
@@ -16,13 +16,17 @@ start = time.time()
 
 #Get hased MD5 input from user
 password = input("Input hashed MD5 password: ")
+password = password.lower()
+password = password.rstrip()
 
 #Open filename from url
 #filename = urllib.urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt')
-filename = str(urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt').read(), 'utf-8')
+filename = str(urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt').read(), 'utf-8')
 
-for guess in filename:
-	hashedGuess = hashlib.sha1(bytes(guess,'utf-8')).hexdigest()
+start = time.time()
+
+for guess in filename.split('\n'):
+	hashedGuess = hashlib.md5(bytes(guess,'utf-8')).hexdigest()
 	#hashedGuess = getattr(hashlib, 'md5')(bytes(guess)).hexdigest()
 	
 
@@ -38,6 +42,9 @@ for guess in filename:
 		print("Password guess: ", str(guess), " does not match, trying next.")
 
 print("Password not found")
+end = time.time()
+t_time = str(round(end - start,3))
+print ("Total runtime was: ", t_time)
 
 '''
 for line in filename:
@@ -51,7 +58,7 @@ for line in filename:
 
 '''
 
-filename.close()
+#filename.close()
 
 
 
