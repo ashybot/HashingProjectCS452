4590535f4d59dadec96218818a41c17f58251dc5
Sammuel Licea <lopez440@cougars.csusm.edu>
2021-05-06 22:43:23 -0700

    Changed code for statistics mode

--- MD5Cracker.py
+++ MD5Cracker.py
@@ -7,15 +7,9 @@ import sys
 from urllib.request import urlopen
 #import codecs
 
-#Start clock
-
-
-#solution = str(sys.argv[1])
-#guess = " "
-#sol = "No Solution Found"
 
 #Get hased MD5 input from user
-password = input("Input hashed MD5 password: ")
+password = input("Input hashed password: ")
 password = password.lower()
 password = password.rstrip()
 
@@ -23,42 +17,40 @@ password = password.rstrip()
 #filename = urllib.urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt')
 filename = str(urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt').read(), 'utf-8')
 
-start = time.time()
+passes = range(100)
 
-for guess in filename.split('\n'):
-	hashedGuess = hashlib.md5(bytes(guess,'utf-8')).hexdigest()
-	#hashedGuess = getattr(hashlib, 'md5')(bytes(guess)).hexdigest()
+for i in passes:
+
+	start = time.time()
+
+	for guess in filename.split('\n'):
+		hashedGuess = hashlib.sha256(bytes(guess,'utf-8')).hexdigest()
+		#hashedGuess = getattr(hashlib, 'md5')(bytes(guess)).hexdigest()
 	
 
-	if hashedGuess == password:
-		print("The password is: ", str(guess))
+		if hashedGuess == password:
+			#print("The password is: ", str(guess))
+			#print(i)
+			end = time.time()
+			t_time = str(round(end - start,2))
+			print(t_time)
+			
+			
+			#print ("Total runtime was: ", t_time)
+			break
 
-		end = time.time()
-		t_time = str(round(end - start,3))
-		print ("Total runtime was: ", t_time)
-		quit()
+	#elif hashedGuess != password:
+	#	print("Password guess: ", str(guess), " does not match, trying next.")
 
-	elif hashedGuess != password:
-		print("Password guess: ", str(guess), " does not match, trying next.")
+#Password not found
+#print("Password not found")
+#end = time.time()
+#t_time = str(round(end - start,2))
+#print ("Total runtime was: ", t_time)
 
-print("Password not found")
-end = time.time()
-t_time = str(round(end - start,3))
-print ("Total runtime was: ", t_time)
 
-'''
-for line in filename:
-	m = hashlib.md5()
-	m.update(line[:-1])
-	guess = m.hexdigest()
 
-	if guess == solution:
-		sol = line
-		break
 
-'''
-
-#filename.close()
 
 
 
